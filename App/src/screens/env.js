7cecd488247fd2bfbfe039aeb7743b33fd0a2a90
Akhil-chaudhary <91989976a@gmail.com>
2020-02-05 21:57:40 +0530

    new

---
+++ App/src/screens/env.js
@@ -0,0 +1,15 @@
+export const dialogflowConfig = 
+{
+    "type": "service_account",
+    "project_id": "citizens-bot-kvkgyt",
+    "private_key_id": "46b2b8a2720e4a714218da574807ac9c7ecc1acf",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC22xU+vcJBq7Xl\nab3ti4gfowkVe0i5BZAkr/sqghunv5Ld9n4N+fNJ2YD2auDgiuD+qjkpJo5kqjny\n7xucXUAKXOg3Z5YwhSzNLgw7MGHUPpq3UoGZSlRlMxwscYtHYZEePxleiIG2WWyb\neFiiZjI6wtkT2KPIv+7m0B+S56AOwCif6a503XyVkFX/595TykBdHi3efP/zDM4i\neFvDiosBq1HT94+Emf3g6v+TR0Qez+mJs1a5CRsS2mx1ANY795UA+mDtaZSdd02R\nudhqpTV5xQfmiM4GTsfWYqJ8RAdAAvq8aD3BTwY2t++uDFR9fvk0tdRD4LuSKQ2p\n5beI2q77AgMBAAECggEACLTCUdwWaAAKnLioeqs+Y5s57ymRMICro0OJvDVjpwdV\nBnfE+q9gPlo6bKvGtzTa5OUqluNSEPLrPWjVsgqH5mEUDAdE5wcq4eiuW7xq/ABb\nEh1oiZQ4vnTCLk+IUnFlHRaOA6qQ2njEK1wohDBEy01u/ns9w8IiWB+n+9ucCver\nKWrx2BmNRyZkVqTJ49tRCd5HF3FdkfFtgP0t0rBGxFlOwlclPtlPiX57x4bHD6UX\nawQn47UMasblcTNQl/JXwwxN5m4GL/tdu4jMei30/lEetHUH4GqWqr9ky+XRkr99\nBlrHKPvr3SrTUonSuY3ET5Zlzcbn9bs8wToFhV2TcQKBgQDw4sBXju+VRWS8Ipgo\nw7bX3G/m64XAgj6HmvY3ZCpF/3NHAXXDwpieKA3HNprTuIIcHN/hP1gdzmlIYfuO\nndTGu2f/SYZ5oJpMLzOphiYgXS+F/F0dhg5ozhOQcMZdzhOHU5+maoYCjk3QRL/m\n76Kk/b11tvkebgE8y4qrOKnfAwKBgQDCVDhp7anb/QicYKaoaQ+ykmdCS6vEhAD+\n7T1g50j1n0v/QX2zT0Oo8onQr9GqXnhSmT74PCog6lxkvkNnIMC78bgLDW1n43B8\nkPGobTnFCwSCXBE+j9smuUUEQ+K6G58fWyBAnLvDYksuCiwG41YCVaaRdQbdxApL\nnu2Dw0LSqQKBgQDSdotu9OfHF/ZHmkIve7BJMcRjkewuop1XhcER7GYy+loR8+ic\nvWzy4jQHlmH4EgE75s6E0v9+K1Y5c8KCfMvnRt46Ylcn9XZakfMlbpvpMwHOowwI\nO5RjZYoabS4jz2WomxumHvQvT34VNbwlXcJRPZFHRDKsr5hNMdlaeAuTWQKBgQCr\nclqDOmdN6yQZszvWATsUNQ5c8x4MNtoaD3zDbcp0yaHnBgS8mgX7nF/yeevk/4vw\nc7uWjp4shBgX2bTl24OvTUcumR4ZNhQcElTiX9tD+MB1snyUUZ7rvzEg3jO+Nnhg\n1ajS2HTfoGzrpZXKTe+oB6D5W+I+7aKXrROn2DGwYQKBgBbbczTgzI/pgh4WsSAt\nkSRmcyJTilL76/+594EGJyv50MZ/yjq7lr6A8aJ381lQHPOTptsgN+71s4BvLwM/\nEykLBax7DSIoLf11WNyJ6mkTfC82qt5eDUPjNeGqHuVUN5aLKmWvLei1AVNEZByM\ncgi07HLTVOyZe4HyVsnx6Xmr\n-----END PRIVATE KEY-----\n",
+    "client_email": "dialogflow-idudgl@citizens-bot-kvkgyt.iam.gserviceaccount.com",
+    "client_id": "108255640027971039824",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/dialogflow-idudgl%40citizens-bot-kvkgyt.iam.gserviceaccount.com"
+  }
+
+  
