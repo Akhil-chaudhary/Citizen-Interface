db69de887d4b49d14fd10427d9d819967d19401f
Akhil Chaudhary <54743359+Akhil-chaudhary@users.noreply.github.com>
2020-02-14 11:32:42 +0530

    Add files via upload

---
+++ App/src/screens/env.js
@@ -0,0 +1,15 @@
+export const dialogflowConfig = 
+{
+    "type": "service_account",
+    "project_id": "citizens-bot-kvkgyt",
+    "private_key_id": "46b2b8a2720e4a714218da574807ac9c7ecc1acf",
+    "private_key": "-----BEGIN PRIVATE KEY-----\\n-----END PRIVATE KEY-----\n",
+    "client_email": "dialogflow-idudgl@citizens-bot-kvkgyt.iam.gserviceaccount.com",
+    "client_id": "108255640027971039824",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/dialogflow-idudgl%40citizens-bot-kvkgyt.iam.gserviceaccount.com"
+  }
+
+  
